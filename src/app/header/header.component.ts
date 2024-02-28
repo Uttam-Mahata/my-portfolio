50951413a14ca0431f3748328e2b5a5fbe560cda
Uttam_02 <mahatauttam0408@gmail.com>
2024-02-28 18:00:28 +0530

    changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { map } from 'rxjs/operators';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,8 +10,27 @@ import { map } from 'rxjs/operators';
 export class HeaderComponent implements OnInit {
   isMobileView = false;
   isNavbarCollapsed = true;
+  isAboutActive: boolean = true;
+  isEducationActive: boolean = false;
+  isSkillsActive: boolean = false;
+  isProjectsActive: boolean = false;
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver, private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Check if the current route is the About route
+        this.isAboutActive = event.urlAfterRedirects === '/';
+        // Check if the current route is the Education route
+        this.isEducationActive = event.urlAfterRedirects === '/education';
+        // Check if the current route is the Skills route
+        this.isSkillsActive = event.urlAfterRedirects === '/skills';
+        // Check if the current route is the Projects route
+        this.isProjectsActive = event.urlAfterRedirects === '/projects';
+
+
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.breakpointObserver.observe([Breakpoints.Handset])
