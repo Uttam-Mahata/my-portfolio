f44693e8a7738123ae5059875c0ab39c19b7aa7a
Uttam_02 <mahatauttam0408@gmail.com>
2024-04-11 08:39:08 +0530

    chnages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,61 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'; // Added HttpClientModule import
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // Added BrowserAnimationsModule import
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { AboutComponent } from './about/about.component';
-import { EducationComponent } from './education/education.component';
-import { SkillsComponent } from './skills/skills.component';
-import { ProjectsComponent } from './projects/projects.component';
+import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
-import { GithubProjectsComponent } from './github-projects/github-projects.component';
-import { GithubService } from './service/github.service';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
-import { ContactComponent } from './contact/contact.component';
 
-import { ButtonModule } from 'primeng/button';
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutComponent,
-    EducationComponent,
-    SkillsComponent,
-    ProjectsComponent,
+    HomeComponent,
     FooterComponent,
-    GithubProjectsComponent,
-    ContactComponent
+ 
   ],
   imports: [
     BrowserModule,
     HttpClientModule, // Added HttpClientModule
     BrowserAnimationsModule, // Added BrowserAnimationsModule
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatCardModule,
-    FontAwesomeModule,
-    RouterModule,
-    AppRoutingModule,
-    ButtonModule,
-    MatSidenavModule,
-    MatListModule,
-    MatMenuModule
+   
   ],
   providers: [
-    GithubService, // Provided GithubService
+   // Provided GithubService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,56 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { map } from 'rxjs/operators';
-import { NavigationEnd, Router } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
 })
-export class HeaderComponent implements OnInit {
-  isMobileView = false;
-  isNavbarCollapsed = true;
-  isAboutActive: boolean = true;
-  isEducationActive: boolean = false;
-  isSkillsActive: boolean = false;
-  isProjectsActive: boolean = false;
-  
-  showMenu:boolean = false;
+export class HeaderComponent {
 
-  constructor(private breakpointObserver: BreakpointObserver, private router: Router) {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        // Check if the current route is the About route
-        this.isAboutActive = event.urlAfterRedirects === '/';
-        // Check if the current route is the Education route
-        this.isEducationActive = event.urlAfterRedirects === '/education';
-        // Check if the current route is the Skills route
-        this.isSkillsActive = event.urlAfterRedirects === '/skills';
-        // Check if the current route is the Projects route
-        this.isProjectsActive = event.urlAfterRedirects === '/projects';
-
-
-      }
-    });
-  }
-
-  ngOnInit(): void {
-    this.breakpointObserver.observe([Breakpoints.Handset])
-      .pipe(map(result => result.matches))
-      .subscribe(isMobile => {
-        this.isMobileView = isMobile;
-        if (this.isMobileView) {
-          this.isNavbarCollapsed = true;
-        }
-      });
-  }
-
-  toggleNavbar(): void {
-    this.isNavbarCollapsed = !this.isNavbarCollapsed;
-  }
-
-
-  toggleMenu(): void {
-    this.showMenu = !this.showMenu;
-  }
 }
