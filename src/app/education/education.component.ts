73f1b3a80cac36fd9c0453621dffa63946d80af0
Uttam_02 <mahatauttam0408@gmail.com>
2024-09-14 20:33:32 +0530

    feat: Add education component and related files

---
+++ src/app/education/education.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+
+interface EducationItem {
+  institution?: string;
+  duration?: string;
+  degree?: string;
+  grade?: string;
+  icon?: string;
+  color?: string;
+  image?: string;
+}
+
+@Component({
+  selector: 'app-education',
+  templateUrl: './education.component.html',
+  styleUrls: ['./education.component.css'],
+})
+export class EducationComponent {
+  educationEvents: EducationItem[];
+
+  constructor() {
+    this.educationEvents = [
+      {
+        institution: 'IIEST, Shibpur',
+        duration: 'Nov 2022 - 2026',
+        degree: 'Bachelor of Technology - BTech, Computer Science and Technology',
+        grade: 'CGPA: 9.07 (Till 4th Semester)',
+        icon: 'pi pi-graduation-cap',
+        color: '#673AB7',
+        image: 'iiest-logo.png',
+      },
+      {
+        institution: 'Bandhgora Anchal Vidyalaya (H.S)',
+        duration: 'WBBSE, 2018',
+        degree: 'Secondary Examination',
+        grade: 'Grade: 93.42%',
+        icon: 'pi pi-book',
+        color: '#9C27B0',
+      },
+      {
+        institution: 'Bandhgora Anchal Vidyalaya (H.S)',
+        duration: 'WBCHSE, 2020',
+        degree: 'Higher Secondary Examination',
+        grade: 'Grade: 92.20%',
+        icon: 'pi pi-book',
+        color: '#FF9800',
+      },
+    ];
+  }
+}
