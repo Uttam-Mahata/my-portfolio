6dde0733dfe55d51e419691f6d75c72de5f79186
Uttam_02 <mahatauttam0408@gmail.com>
2024-09-14 19:12:59 +0530

    feat: Add new components and update component HTML files

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent implements OnInit {
+  
+  items: any[];
+
+  ngOnInit() {
+    this.items = [
+      { label: 'Home', icon: 'pi pi-fw pi-home', routerLink: '/' },
+      // Education
+      { label: 'Education', icon: 'pi pi-fw pi-book', routerLink: '/education' },
+      // Projects
+      { label: 'Projects', icon: 'pi pi-fw pi-briefcase', routerLink: '/projects' },
+      { label: 'Skills', icon: 'pi pi-fw pi-cog', routerLink: '/skills' },
+      { label: 'Experience', icon: 'pi pi-fw pi-file', routerLink: '/experience' },
+      { label: 'Contact', icon: 'pi pi-fw pi-envelope', routerLink: '/contact' },
+    ];
+  }
+
+}
